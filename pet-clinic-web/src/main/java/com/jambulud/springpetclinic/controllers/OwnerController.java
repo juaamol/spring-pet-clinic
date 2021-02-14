50c1aebd786256d659a8eb4e822594f169fa42ad
juaamol <jambulud@gmail.com>
2021-02-14 11:36:34 +0100

    Owners controller and page

---
+++ pet-clinic-web/src/main/java/com/jambulud/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.jambulud.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String owners() {
+        return "owner/index";
+    }
+
+}

00614d84f6d2f5add61b8868980574316e472450
juaamol <jambulud@gmail.com>
2021-02-19 21:50:46 +0100

    List of vets

--- pet-clinic-web/src/main/java/com/jambulud/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jambulud/springpetclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.jambulud.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jambulud.springpetclinic.services.VetService;
 
 @Controller
 public class VetController {
-    
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String vets() {
-        return "vet/index";
-    }
+
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+
+    this.vetService = vetService;
+  }
+
+  @RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+  public String vets(Model model) {
+
+    model.addAttribute("vets", this.vetService.findAll());
+
+    return "vet/index";
+  }
 }

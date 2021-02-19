a6c4b502032ec1a596162ef080725a4b329bb765
juaamol <jambulud@gmail.com>
2021-02-19 21:33:21 +0100

    Owners list

--- pet-clinic-web/src/main/java/com/jambulud/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/jambulud/springpetclinic/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package com.jambulud.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jambulud.springpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String owners() {
-        return "owner/index";
-    }
+  private final OwnerService ownerService;
+
+  public OwnerController(OwnerService ownerService) {
+
+    this.ownerService = ownerService;
+  }
+
+  @RequestMapping({ "", "/", "/index", "/index.html" })
+  public String owners(Model model) {
+
+    model.addAttribute("owners", this.ownerService.findAll());
+
+    return "owner/index";
+  }
 
 }
